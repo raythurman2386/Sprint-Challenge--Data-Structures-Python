5745b1853472f7e1e1defdf49608911fda3e1c97
Raymond Thurman <raymondthurman5@gmail.com>
2020-06-28 22:00:35 -0400

    added list comp in names

--- names/names.py
+++ names/names.py
@@ -11,14 +11,16 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+# Return the list of duplicates in this data structure
+# duplicates = [name for name in names_1 if name in names_2]
+duplicates = []
 
 bst = BSTNode('testing')
 
 
-# Not quite sure how else to accomplish this at the moment
-# OPTIMIZED TO RUN IN 0.172 SECONDS ON MY MACHINE
-# for each name in names_1
+# # Not quite sure how else to accomplish this at the moment
+# # OPTIMIZED TO RUN IN 0.172 SECONDS ON MY MACHINE
+# # for each name in names_1
 for name in names_1:  # O(n)
     # add that name to the binary search tree
     bst.insert(name)
@@ -30,13 +32,13 @@ for name in names_2:
         # if it is, append it to the duplicates array
         duplicates.append(name)
 
-    # Replace the nested for loops below with your improvements
-    # RUNTIME IS 7.6484 SECONDS ON MY MACHINE
-    # Current runtime = O(n) + O(n) + O(1) + O(1) == O(n^2)
-    # for name_1 in names_1:  # O(n)
-    #     for name_2 in names_2:  # O(n)
-    #         if name_1 == name_2:  # O(1)
-    #             duplicates.append(name_1)  # O(1)?
+# Replace the nested for loops below with your improvements
+# RUNTIME IS 7.6484 SECONDS ON MY MACHINE
+# Current runtime = O(n) + O(n) + O(1) + O(1) == O(n^2)
+# for name_1 in names_1:  # O(n)
+#     for name_2 in names_2:  # O(n)
+#         if name_1 == name_2:  # O(1)
+#             duplicates.append(name_1)  # O(1)?
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
